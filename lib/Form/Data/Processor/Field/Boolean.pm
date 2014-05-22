db3e0b48f6daab76dc2bc3c073a8b0454c205015
Dmitry Latin <dim0xff@gmail.com>
2014-05-22 14:11:10 +0400

    Fix pod for FDP::Field::Boolean, remove required_input

--- lib/Form/Data/Processor/Field/Boolean.pm
+++ lib/Form/Data/Processor/Field/Boolean.pm
@@ -2,25 +2,14 @@ package Form::Data::Processor::Field::Boolean;
 
 =head1 NAME
 
-Form::Data::Processor::Field::Boolean - bool field
+Form::Data::Processor::Field::Boolean - boolean field
 
 =cut
-
-use utf8;
-
-use strict;
-use warnings;
 
 use Form::Data::Processor::Moose;
 use namespace::autoclean;
 
 extends 'Form::Data::Processor::Field';
-
-sub BUILD {
-    my $self = shift;
-
-    $self->set_error_message( required_input => 'Value is not provided', );
-}
 
 has force_result => (
     is      => 'rw',
@@ -34,31 +23,16 @@ has real_result => (
     default => 0,
 );
 
-has required_input => (
-    is      => 'rw',
-    isa     => 'Bool',
-    default => 0,
-);
 
-after _before_ready => sub {
+sub BUILD {
     my $self = shift;
 
     $self->set_default_value(
-        required_input => $self->required_input,
-        real_result    => $self->real_result,
-        force_result   => $self->force_result,
+        real_result  => $self->real_result,
+        force_result => $self->force_result,
     );
-};
+}
 
-around validate => sub {
-    my $orig = shift;
-    my $self = shift;
-
-    $self->$orig();
-
-    return $self->add_error('required_input')
-        if $self->required_input && !$self->validate_required_input();
-};
 
 around validate_required => sub {
     my $orig = shift;
@@ -70,13 +44,6 @@ around validate_required => sub {
     return 1;
 };
 
-sub validate_required_input {
-    my $self = shift;
-
-    return 1 if $self->has_value;
-
-    return 0;
-}
 
 sub has_result {
     my $self = shift;
@@ -87,6 +54,7 @@ sub has_result {
     return $self->has_value;
 }
 
+
 sub _result {
     my $self = shift;
 
@@ -94,11 +62,26 @@ sub _result {
     return ( $self->value ? 1 : 0 );
 }
 
+
 __PACKAGE__->meta->make_immutable;
+
 
 1;
 
 __END__
+
+=head1 SYNOPSIS
+
+    package My::Form::Search;
+    use Form::Data::Processor::Moose;
+    extends 'Form::Data::Processor::Form';
+
+    has_field agree_license     => ( type => 'Boolean', required     => 1 );
+    has_field search_with_photo => ( type => 'Boolean', force_result => 1 );
+
+    # Addition fields for search
+    ...
+
 
 =head1 DESCRIPTION
 
@@ -106,30 +89,12 @@ This field represent boolean data.
 
 This field is directly inherited from L<Form::Data::Processor::Field>.
 
-Field sets own error messages:
-
-    'required_input' => 'Value is not provided',
-
-
-=head1 SYNOPSYS
-
-    package My::Form::Search;
-    use Form::Data::Processor::Moose;
-
-    extends 'Form::Data::Processor::Form';
-
-    has_field agree_license     => ( type => 'Boolean', required => 1 );
-    has_field search_with_photo => ( type => 'Boolean' );
-
-    # Addition fields for search
-    ...
-
 
 =head1 ACCESSORS
 
 Other accessors can be found in L<Form::Data::Processor::Field/ACCESSORS>
 
-All local accessors will be resettable.
+B<Notice:> all current accessors will be resettable.
 
 
 =head2 force_result
@@ -142,8 +107,9 @@ All local accessors will be resettable.
 
 =back
 
-When C<true>, then field has result when input value is not provided, and result is C<0>.
-Otherwise there is no result for this field, when input value is not provided.
+If C<true>, then field has result when input value is not provided, and result
+is C<0>. Otherwise there is no result for this field, when input value is not
+provided.
 
 
 =head2 real_result
@@ -156,31 +122,14 @@ Otherwise there is no result for this field, when input value is not provided.
 
 =back
 
-When C<false>, then field L<Form::Data::Processor::Field/result> will be C<0> or C<1>.
-Otherwise it returns provided value.
+When C<false>, then field L<Form::Data::Processor::Field/result> will be C<0>
+or C<1>. Otherwise it returns provided value.
 
 
 =head2 required
 
-After field input value passed L<Form::Data::Processor::Field/required> test,
-it has one more required test - test for boolean required.
+Field value should not be empty (C<undef>, C<0>, C<''> etc.). Also see
+L<Form::Data::Processor::Field/required>.
 
-Boolean required test is passed when field value is not empty
-(C<undef>, C<0>, C<''> etc.).
-
-
-=head2 required_input
-
-=over 4
-
-=item Type: Bool
-
-=item Default: false
-
-=back
-
-If set to C<true>, then value for field MUST be provided (in any form).
-If value is not provided for required input field, then error C<required_input>
-will be added to field errors.
 
 =cut
