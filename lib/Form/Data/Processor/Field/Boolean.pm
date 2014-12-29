be1b4667ad476889e1840d024ec3e9df93de2292
Dmitry Latin <dim0xff@gmail.com>
2014-12-29 13:43:31 +0300

    Remove `real_result`
    It is really strange feature

--- lib/Form/Data/Processor/Field/Boolean.pm
+++ lib/Form/Data/Processor/Field/Boolean.pm
@@ -13,18 +13,11 @@ has force_result => (
     default => 0,
 );
 
-has real_result => (
-    is      => 'rw',
-    isa     => 'Bool',
-    default => 0,
-);
-
 
 after populate_defaults => sub {
     my $self = shift;
 
     $self->set_default_value(
-        real_result  => $self->real_result,
         force_result => $self->force_result,
     );
 };
@@ -61,7 +54,6 @@ sub result {
 sub _result {
     my $self = shift;
 
-    return $self->value if $self->real_result;
     return ( $self->value ? 1 : 0 );
 }
 
@@ -108,20 +100,6 @@ B<Notice:> all current attributes are resettable.
 If C<true>, then field has result when input value is not provided, and result
 is C<0>. Otherwise there is no result for this field, when input value is not
 provided.
-
-
-=attr real_result
-
-=over 4
-
-=item Type: Bool
-
-=item Default: false
-
-=back
-
-When C<false>, then field L<Form::Data::Processor::Field/result> will be C<0>
-or C<1>. Otherwise it returns provided value.
 
 
 =attr required
