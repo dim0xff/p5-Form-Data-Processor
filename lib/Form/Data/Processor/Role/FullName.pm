9370f58655565b012264e7c6600a0bf3efbcfa2f
Dmitry Latin <dim0xff@gmail.com>
2016-12-09 23:13:23 +0300

    Changes according to Roles manual

--- lib/Form/Data/Processor/Role/FullName.pm
+++ lib/Form/Data/Processor/Role/FullName.pm
@@ -3,7 +3,6 @@ package Form::Data::Processor::Role::FullName;
 # ABSTRACT: role provides C<full_name> attribute and generator
 
 use Moose::Role;
-use namespace::autoclean;
 
 requires 'name', 'parent', 'has_parent';
 
